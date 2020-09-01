e59a8571864584cc694d23516ef40171b0cf9371
Marciovesk <69768206+Marciovesk@users.noreply.github.com>
2020-09-01 16:21:03 -0300

    1.2
    Adicionando uma nova function de criar a linha

--- public/js/main.js
+++ public/js/main.js
@@ -79,24 +79,42 @@ function finalizaJogo(){
 
 function inserePlacar(){
     var corpoTabela = $(".placar").find("tbody");
-    var usuario = "José";
+    var usuario = "Douglas";
     var numPalavras = $("#contador-palavras").text();
-    var botaoRemover ="<a href='#'><i class='small material-icons'>delete</i></a>";
 
-    var linha = "<tr>"+
-                    "<td>"+ usuario + "</td>"+
-                    "<td>"+ numPalavras + "</td>"+
-                    "<td>"+ botaoRemover + "</td>"+
-                "</tr>";
+    var linha = novaLinha(usuario,numPalavras);
+    linha.find(".botao-remover").click(removeLinha);
 
-    corpoTabela.append(linha);
+    //Adiciona a linha como a mais recente na lista do placar.
+    corpoTabela.prepend(linha);
 }
 
+function novaLinha(usuario,palavras){
 
-$(".botao-remover").click(function(event){
+	var linha = $("<tr>");
+	var colunaUsuario = $("<td>").text(usuario);
+	var colunaPalavras = $("<td>").text(palavras);
+	var colunaRemover = $("<td>");
+
+	var link = $("<a>").addClass("botao-remover").attr("href","#");
+	var icone = $("<i>").addClass("small").addClass("material-icons").text("delete");
+
+	link.append(icone);
+
+	colunaRemover.append(link);
+
+	linha.append(colunaUsuario);
+	linha.append(colunaPalavras);
+	linha.append(colunaRemover);
+
+	return linha;
+
+}
+
+function removeLinha(){
 	event.preventDefault();
 	$(this).parent().parent().remove();
-});
+}
 
 
 function reiniciaJogo(){
